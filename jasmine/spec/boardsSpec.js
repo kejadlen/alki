e1f9405a84e28d67b51f930caba11e82dd1be6da
Alpha and Steve Gravrock <pair+achen+sgravrock@pivotal.io>
2016-01-12 11:00:37 -0800

    Add specs around clicking Stop

--- jasmine/spec/boardsSpec.js
+++ jasmine/spec/boardsSpec.js
@@ -5,7 +5,7 @@ describe("Boards", function () {
         spyOn($, "post");
         spyOn($, "ajax");
 
-        this.$root = $('<div><a class="start" id="foo"></a><a class="stop"></a></div>');
+        this.$root = $('<div><a class="start" id="foo"></a><a class="stop" id="bar"></a></div>');
         this.subject = new Alki.boards(this.$root);
         spyOn(this.subject, "reload");
         this.subject.init();
@@ -27,6 +27,30 @@ describe("Boards", function () {
         describe("on a successful call to webhook", function () {
             beforeEach(function () {
                 $.post.calls.mostRecent().args[2]();
+            });
+
+            it("should reload the page", function() {
+                expect(this.subject.reload).toHaveBeenCalled();
+            });
+        });
+    });
+
+    describe("When the user clicks Stop", function () {
+        beforeEach(function () {
+            this.$root.find(".stop").click();
+        });
+
+        it("should make an ajax request to webhook", function () {
+            var args;
+            expect($.ajax).toHaveBeenCalled();
+            args = $.ajax.calls.mostRecent().args[0];
+            expect(args.type).toBe("DELETE");
+            expect(args.url).toEqual("webhook/bar");
+        });
+
+        describe("on a successful call to webhook", function () {
+            beforeEach(function () {
+                $.ajax.calls.mostRecent().args[0].success();
             });
 
             it("should reload the page", function() {
