20d33cc9786e704839c028ff6e9691205024fcfb
Alpha Chen and Sarah McAlear <pair+achen+smcalear@pivotal.io>
2016-02-03 11:08:49 -0800

    Rename cards to stats in test_stats

--- test/test_stats.rb
+++ test/test_stats.rb
@@ -24,20 +24,20 @@ class TestStats < Alki::Test
        "date" => "2016-01-03T01:00:00.000Z",
        "data" => {"card" => {"id" => "2"}, "listAfter" => {"id" => "yet_another_list_id"}}},
     ]
-    @card = Stats.new(actions: actions, time: Time.parse("2016-01-08T08:00:00Z"))
+    @stats = Stats.new(actions: actions, time: Time.parse("2016-01-08T08:00:00Z"))
   end
 
   def test_averages
     assert_equal({ "some_list_id" => 129_600,
                    "another_list_id" => 172_800,
-                   "yet_another_list_id" => 358_200 }, @card.averages)
+                   "yet_another_list_id" => 358_200 }, @stats.averages)
   end
 
   def test_wait_times
     assert_equal({ "some_list_id" => 86_400,
                    "another_list_id" => 172_800,
-                   "yet_another_list_id" => 259_200 }, @card.wait_times["1"])
+                   "yet_another_list_id" => 259_200 }, @stats.wait_times["1"])
     assert_equal({ "some_list_id" => 172_800,
-                   "yet_another_list_id" => 457_200 }, @card.wait_times["2"])
+                   "yet_another_list_id" => 457_200 }, @stats.wait_times["2"])
   end
 end
